0358e6147e6a678a93cc5b2d2e359f910f4ecb51
Sergey <sergey_tkachev_95@mail.ru>
2022-04-03 20:32:37 +0300

    homework2_2

---
+++ HomeWork2_2.py
@@ -0,0 +1,24 @@
+# Задание №2 необходимо
+# обработать список.
+temp = ['в', '5', 'часов', '17', 'минут',
+        'температура', 'воздуха',
+        'была', '+5', 'градусов']
+
+a = []
+
+
+temp.remove('5')
+temp.remove('+5')
+
+temp.insert(1, '05')
+temp.insert(8, '+05')
+
+temp.insert(1, '""')
+temp.insert(3, '""')
+temp.insert(5, '""')
+temp.insert(7, '""')
+temp.insert(12, '""')
+temp.insert(14, '""')
+print(temp)
+
+print(' '.join(temp))
